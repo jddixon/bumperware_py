2a20045aea329c7392bd6a56075644fdad08d8a9
Jim Dixon <jddixon@gmail.com>
2017-08-30 11:39:05 -0700

    add tox.ini, *requirements.txt; minor edits to config files

--- src/bumperware/__init__.py
+++ src/bumperware/__init__.py
@@ -2,7 +2,7 @@
 
 """ default proj desc """
 
-__version__ = '0.0.4'
-__version_date__ = '2017-07-12'
+__version__ = '0.0.5'
+__version_date__ = '2017-08-30'
 
 __all__ = ['__version__', '__version_date__']
